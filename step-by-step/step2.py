48616684b3cd649daaacdd1b8d4d27b8a9e225f9
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2019-09-10 14:28:53 +0100

    Create step2.py

---
+++ step-by-step/step2.py
@@ -0,0 +1,35 @@
+from microbit import *
+#Instructables Step 4
+
+#import radio
+
+#radio.config(channel=_, group=_, queue=1)
+#radio.on()
+
+'''
+INPUTS
+joystick:bit
+'''
+buttons = {2: 'A',
+           516: 'B',
+           684: 'C',
+           768: 'D',
+           853: 'E',
+           819: 'F'}
+
+def button_press():
+    press = pin2.read_analog()
+    if press < 900:
+        for number in range(press-5, press+5):
+            if number in buttons:
+                return buttons[number]
+
+def joystick_push():
+    x = pin0.read_analog() - 518
+    y = pin1.read_analog() - 523
+    return x, y
+
+while True:
+    joystick = joystick_push()
+    print(joystick)
+    sleep(500)
